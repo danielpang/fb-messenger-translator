6c2f3437bd470c2c28df7f08ca70dd08b3087aac
Daniel Pang <hdpang@uwaterloo.ca>
2018-01-18 18:45:13 -0500

    Validates messages

--- app.js
+++ app.js
@@ -88,44 +88,30 @@ app.get('/webhook', (req, res) => {
   }
 });
 
+function validMessage(message){
+	var ind = message.indexof(':');
+	if (ind <= 0){
+		return false;
+	} else {
+		var target_lang = message.substring(0, ind + 1).toLowerCase();
+		if (target_lang == 'french' || target_lang == 'german'){
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
+
 function handleMessage(sender_psid, received_message) {
   let response;
 
-  // Checks if the message contains text
-  if (received_message.text) {
-    // Create the payload for a basic text message, which
-    // will be added to the body of our request to the Send API
+  // Checks if the message contains text and is valid
+  if (received_message.text && validMessage(recevied_message.text)) {
     response = {
-      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+      "text": `HI!`
     }
-  } else if (received_message.attachments) {
-    // Get the URL of the message attachment
-    let attachment_url = received_message.attachments[0].payload.url;
-    response = {
-      "attachment": {
-        "type": "template",
-        "payload": {
-          "template_type": "generic",
-          "elements": [{
-            "title": "Is this the right picture?",
-            "subtitle": "Tap a button to answer.",
-            "image_url": attachment_url,
-            "buttons": [
-              {
-                "type": "postback",
-                "title": "Yes!",
-                "payload": "yes",
-              },
-              {
-                "type": "postback",
-                "title": "No!",
-                "payload": "no",
-              }
-            ],
-          }]
-        }
-      }
-    }
+  } else {
+
   }
 
   // Send the response message

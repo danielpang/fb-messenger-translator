c4df85a2f8100beef97aeaca5af648e4027f5eac
Daniel Pang <hdpang@uwaterloo.ca>
2018-01-18 02:22:16 -0500

    Setup

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ const
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 5000, () => console.log('webhook is listening'));
 
+// Server index page
+app.get("/", function (req, res) {
+  res.send("Deployed!");
+});
+
 // Accepts POST requests at /webhook endpoint
 app.post('/webhook', (req, res) => {
 

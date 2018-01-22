a58d99f961749a3c219ff84f8a4bdf91cc80f1ca
Daniel Pang <hdpang@uwaterloo.ca>
2018-01-21 19:22:31 -0500

    Fixes

--- app.js
+++ app.js
@@ -174,9 +174,9 @@ function translate_message(target_lang, message){
       "uri": "https://translation.googleapis.com/language/translate/v2?key=" + GOOGLE_API_KEY,
       "method": "POST",
       "json": request_body
-  }, (err, results, body) => {
+  }, {json: true}, (err, results, body) => {
       if (!err) {
-        console.log(results.translations)
+        console.log(data.translations);
       } else {
         console.error("Unable to translate, error message:" + err);
       }

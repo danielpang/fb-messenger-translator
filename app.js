e6433b57b34977e102de175f3e0703f66cc4a2db
Daniel Pang <hdpang@uwaterloo.ca>
2018-01-21 21:15:12 -0500

    Fixes

--- app.js
+++ app.js
@@ -169,7 +169,7 @@ function callSendAPI(sender_psid, response) {
     "json": request_body
   }, (err, res, body) => {
     if (!err) {
-      console.log('message sent!!')
+      console.log('message: ' + response['text'] +  'sent!!')
     } else {
       console.error("Unable to send message:" + err);
     }
